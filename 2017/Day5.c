50d442b2f96fdaf4606718606c1879a42d51d245
Alexander Dewfall <seraphofthemaelstrom@googlemail.com>
2017-12-05 08:39:50 +0000

    Day 5 completed.

---
+++ 2017/Day5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int solver(int a[], int l);
+int runner(FILE* f, int a[]);
+int main(int argc, char** argv)
+{
+    FILE* f = fopen(argv[1], "r");
+    int a[1024];
+    int l;
+    l = runner(f, a);
+    printf("%d\n", solver(a,l));
+    return 0;
+}
+
+int runner(FILE* f, int a[])
+{
+    char* b = (char*)malloc(16);
+    int l = 0;
+    if (a == NULL || b == NULL)
+        exit(1);
+    while(!feof(f))
+    {
+        fgets(b,16,f);
+        a[l] = strtol(b,NULL,10);
+        l++;
+    }
+    return l;
+}
+
+int solver(int a[], int l)
+{
+    int i, s;
+    i = s = 0;
+    while (i >= 0 && i < l)
+    {
+        i += a[i]++;
+        s++;
+    }
+    return s;
+}
