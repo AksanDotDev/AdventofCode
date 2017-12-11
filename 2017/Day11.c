c406a0ff36920321dfdb1496d4f852f1f0ff6210
Alexander Dewfall <seraphofthemaelstrom@googlemail.com>
2017-12-11 08:52:06 +0000

    Day11 done.

---
+++ 2017/Day11.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int runner(FILE*);
+void add_step(char*, int*, int*);
+
+int main(int argc, char** argv)
+{
+    FILE* f = fopen(argv[1], "r");
+    printf("%d\n", runner(f));
+    fclose(f);
+    return 0;
+}
+
+int runner(FILE* f)
+{
+    char* b = (char*)calloc(1,4);
+    int i, x, y, s;
+    char c;
+    if (b == NULL)
+        exit(1);
+    x = y = s = i = 0;
+    while(!feof(f))
+    {
+        c = fgetc(f);
+        switch(c)
+        {
+            case ',' :
+                b[i] = 0;
+                add_step(b,&x,&y);
+                i = 0;
+                break;
+            default :
+                b[i++] = c;
+                break;
+        }
+    }
+    b[i] = 0;
+    add_step(b,&x,&y);
+    free(b);
+    if (abs(y) < (abs(y) + abs((abs(x)-abs(y)))/2))
+        s = abs(y);
+    else
+        s = abs(y) + abs((abs(x)-abs(y)))/2;
+    return(s);
+}
+
+void add_step(char* b, int* x, int* y)
+{
+    switch (b[0])
+    {
+        case 'n' :
+            *x += 1;
+            break;
+        case 's' :
+            *x -= 1;
+            break;
+        default :
+            fprintf(stderr, "Error\n");
+            break;
+    }
+    switch (b[1])
+    {
+        case 'e' :
+            *y += 1;
+            break;
+        case 'w' :
+            *y -= 1;
+            break;
+        default :
+            switch (b[0])
+        {
+            case 'n' :
+                *x += 1;
+                break;
+            case 's' :
+                *x -= 1;
+                break;
+            default :
+                fprintf(stderr, "Error2\n");
+                break;
+        }
+    }
+}
