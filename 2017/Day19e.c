c7f7abca46452044c8fb8b4a727a9dfaae84980e
Alexander Dewfall <seraphofthemaelstrom@googlemail.com>
2017-12-19 12:20:48 +0000

    Day 19 done.

---
+++ 2017/Day19e.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char** map;
+int xlim;
+int ylim;
+int result;
+
+void reader(FILE*);
+void runner(void);
+
+int main(int argc, char** argv)
+{
+    int i;
+    FILE* f = fopen(argv[1], "r");
+    reader(f);
+    fclose(f);
+    runner();
+    for (i=0;i<ylim;i++)
+        free(map[i]);
+    free(map);
+    printf("%d\n", result);
+    return 0;
+}
+
+
+void reader(FILE* f)
+{
+    char* buffer = (char*)malloc(512);
+    int i;
+    i = 0;
+    if (!buffer)
+    {
+        fprintf(stderr, "Malloc failed\n");
+        exit(1);
+    }
+    while(!feof(f))
+    {
+        fgets(buffer,512,f);
+        i++;
+    }
+    ylim = i;
+    xlim = strlen(buffer);
+    rewind(f);
+    map = malloc(ylim*sizeof(char*));
+    if (!map)
+    {
+        fprintf(stderr, "Malloc failed\n");
+        exit(1);
+    }
+    for (i=0;i<ylim;i++)
+    {
+        map[i] = malloc((xlim+1)*sizeof(char));
+        if (!map[i])
+        {
+            fprintf(stderr, "Malloc failed\n");
+            exit(1);
+        }
+        fgets(buffer,512,f);
+        strcpy(map[i],buffer);
+    }
+    free(buffer);
+}
+
+void runner(void)
+{
+    int x, y, d; // D = 0, L = 1, U = 2, R = 3
+    y = d = 0;
+    result = 1;
+    for (x=0;x<xlim;x++)
+        if (map[0][x] == '|')
+            break;
+    while (1)
+    {
+        switch (d)
+        {
+            case 0 :
+                y++;
+                break;
+            case 1 :
+                x--;
+                break;
+            case 2 :
+                y--;
+                break;
+            case 3 :
+                x++;
+                break;
+            default :
+                fprintf(stderr, "Corrupted direction.\n");
+                break;
+        }
+        if (x < 0 || y < 0 || x >= xlim || y >= ylim || map[y][x] == ' ')
+            break;
+        switch (map[y][x])
+        {
+            case '+' :
+                if (d % 2)
+                {
+                    if (y+1 < ylim && map[y+1][x] != ' ')
+                        d = 0;
+                    else 
+                        d = 2;
+                }
+                else 
+                {
+                    if (x+1 < xlim && map[y][x+1] != ' ')
+                        d = 3;
+                    else 
+                        d = 1;
+                }
+                break;
+            default :
+                break;
+        }
+        result++;
+    }
+}
