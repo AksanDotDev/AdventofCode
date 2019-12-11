abcd941ba610ee21af94d73561d2ec523c67d28b
Alexander Dewfall <seraphofthemaelstrom@googlemail.com>
2019-12-11 08:52:51 +0000

    Trust noone, paint everything.

--- 2019/Day11.py
+++ 2019/Day11.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 
 def em_adjust_base(code, index, base, par_str):
     if (par_str[0] == '0'):
@@ -84,7 +85,7 @@ def em_jump_if_true(code, index, base, par_str):
 
     return (arg_1 if arg_0 != 0 else index + 3), base
 
-def em_output(code, index, base, par_str):
+def em_output(code, index, base, par_str, painting, canvas):
     if (par_str[0] == '0'):
         arg_0 = referencedValue(code, index + 1)
     elif (par_str[0] == '2'):
@@ -92,14 +93,20 @@ def em_output(code, index, base, par_str):
     else:
         arg_0 = directValue(code, index + 1)
 
-    print(arg_0)
-    return (index + 2), base
-
-def em_input(code, index, base, par_str):
-    if (par_str[2] =='0'):
-        writeReference(code, index + 1, input())
+    if (painting):
+        paint(arg_0, canvas)
+        painting = False
     else:
-        writeRelative(code, index + 1, base, input())
+        move(arg_0, canvas)
+        painting = True
+    
+    return (index + 2), base, painting, canvas
+
+def em_input(code, index, base, par_str, canvas):
+    if (par_str[2] =='0'):
+        writeReference(code, index + 1, observe(canvas))
+    else:
+        writeRelative(code, index + 1, base, observe(canvas))
     return (index + 2), base
 
 def em_multiplication(code, index, base, par_str):
@@ -109,6 +116,7 @@ def em_multiplication(code, index, base, par_str):
         arg_0 = relativeValue(code, index + 1, base)
     else:
         arg_0 = directValue(code, index + 1)
+
     if (par_str[1] == '0'):
         arg_1 = referencedValue(code, index + 2)
     elif (par_str[1] == '2'):
@@ -130,6 +138,7 @@ def em_addition(code, index, base, par_str):
         arg_0 = relativeValue(code, index + 1, base)
     else:
         arg_0 = directValue(code, index + 1)
+
     if (par_str[1] == '0'):
         arg_1 = referencedValue(code, index + 2)
     elif (par_str[1] == '2'):
@@ -159,7 +168,29 @@ def writeReference(code, index, value):
 def writeRelative(code, index, base, value):
     code[base + code[index]] = value
 
-def emulate(code, index, base):
+def paint(colour, canvas):
+    x, y, d = canvas["loc"]
+    canvas["("+str(x)+","+str(y)+")"] = '█' if (int(colour) == 0) else ' '
+
+def move(direction, canvas):
+    x, y, d = canvas["loc"]
+    d = (d + (1 if int(direction) == 1 else -1)) % 4
+    if (d == 0):
+        y -= 1
+    elif (d == 1):
+        x += 1
+    elif (d == 2):
+        y += 1
+    elif (d == 3):
+        x -= 1
+    canvas["loc"] = (x, y, d)
+
+def observe(canvas):
+    x, y, d = canvas["loc"]
+    return 1 if (canvas["("+str(x)+","+str(y)+")"] == ' ') else 0
+    
+
+def emulate(code, index, base, painting, canvas):
     op_str = str(code[index]).zfill(5)
     op = int(op_str[3:])
     par_str = op_str[2::-1]
@@ -176,9 +207,9 @@ def emulate(code, index, base):
     elif (op == 5):
         index, base = em_jump_if_true(code, index, base, par_str)
     elif (op == 4):
-        index, base = em_output(code, index, base, par_str)
+        index, base, painting, canvas = em_output(code, index, base, par_str, painting, canvas)
     elif (op == 3):
-        index, base = em_input(code, index, base, par_str)
+        index, base = em_input(code, index, base, par_str, canvas)
     elif (op == 2):
         index, base = em_multiplication(code, index, base, par_str)
     elif (op == 1):
@@ -186,15 +217,20 @@ def emulate(code, index, base):
     else:
         print(">>><<<")
         index = -1
-    return index, base
+    return index, base, painting, canvas
 
 raw_intcode = input()
 str_intcode = raw_intcode.split(',')
 intcode = list(map(int, str_intcode))
-for i in range (0,128):
+for i in range (0,1024):
     intcode.append(0)
 
 i = 0
 b = 0
+p = True
+c = defaultdict(lambda: '█')
+c["loc"] = (0,0,0)
 while (i != -1):
-    i, b = emulate(intcode, i, b)
+    i, b, p, c = emulate(intcode, i, b, p, c)
+
+print(len(c) - 1)
