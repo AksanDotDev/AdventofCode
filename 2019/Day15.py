c1b04202e8c14f9166c94f50235265181a2aa41e
Alexander Dewfall <seraphofthemaelstrom@googlemail.com>
2019-12-15 13:01:19 +0000

    Wandering aimlessly in the dark, oh and code.

--- 2019/Day15.py
+++ 2019/Day15.py
@@ -1,5 +1,6 @@
 from collections import defaultdict, Counter
 from time import sleep
+from random import randint
 
 def em_adjust_base(code, index, base, par_str):
     if (par_str[0] == '0'):
@@ -86,7 +87,7 @@ def em_jump_if_true(code, index, base, par_str):
 
     return (arg_1 if arg_0 != 0 else index + 3), base
 
-def em_output(code, index, base, par_str, screen):
+def em_output(code, index, base, par_str, log):
     if (par_str[0] == '0'):
         arg_0 = referencedValue(code, index + 1)
     elif (par_str[0] == '2'):
@@ -94,16 +95,21 @@ def em_output(code, index, base, par_str, screen):
     else:
         arg_0 = directValue(code, index + 1)
 
-    screen.append(arg_0)
-    
+    move, placeholder = log[-1]
+    log[-1] = (move, arg_0)
+    count = process(log)
+    if arg_0 == 2 :
+        print(count)
+        exit()
     return (index + 2), base
 
-def em_input(code, index, base, par_str, screen):
+def em_input(code, index, base, par_str, log):
+    move, placeholder = log[-1]
 
     if (par_str[2] =='0'):
-        writeReference(code, index + 1, 0)
+        writeReference(code, index + 1, move)
     else:
-        writeRelative(code, index + 1, base, 0)
+        writeRelative(code, index + 1, base, move)
 
     return (index + 2), base
 
@@ -166,7 +172,52 @@ def writeReference(code, index, value):
 def writeRelative(code, index, base, value):
     code[base + code[index]] = value
 
-def emulate(code, index, base, screen):
+def process(log):
+    map = defaultdict(lambda: ' ')
+    crumbs = 0
+    x_loc = 0
+    y_loc = 0
+    for move, result in log:
+        if int(move) == 1:
+            x_pot = x_loc
+            y_pot = y_loc - 1
+        elif int(move) == 2:
+            x_pot = x_loc
+            y_pot = y_loc + 1
+        elif int(move) == 3:
+            x_pot = x_loc - 1
+            y_pot = y_loc
+        elif int(move) == 4:
+            x_pot = x_loc + 1
+            y_pot = y_loc
+        else:
+            print("Bad move")
+            exit()
+        if int(result) == 0:
+            map[(x_pot, y_pot)] = '█'
+        elif int(result) == 1:
+            if map[(x_pot, y_pot)] == '°':
+                map[(x_loc, y_loc)] = '■'
+                crumbs -= 1
+            else:
+                map[(x_pot, y_pot)] = '°'
+                crumbs += 1
+            x_loc = x_pot
+            y_loc = y_pot
+        else:
+            map[(x_pot, y_pot)] = 'O'
+            crumbs += 1
+    options = [map[(x_loc, y_loc - 1)], map[(x_loc, y_loc + 1)], map[(x_loc - 1, y_loc)], map[(x_loc + 1, y_loc)]]
+    try:
+        next = options.index(' ') + 1
+    except ValueError:
+        next = options.index('°') + 1
+    finally:
+        log.append((next, -1))
+    return crumbs
+
+
+def emulate(code, index, base, log):
     op_str = str(code[index]).zfill(5)
     op = int(op_str[3:])
     par_str = op_str[2::-1]
@@ -183,9 +234,9 @@ def emulate(code, index, base, screen):
     elif (op == 5):
         index, base = em_jump_if_true(code, index, base, par_str)
     elif (op == 4):
-        index, base = em_output(code, index, base, par_str, screen)
+        index, base = em_output(code, index, base, par_str, log)
     elif (op == 3):
-        index, base = em_input(code, index, base, par_str, screen)
+        index, base = em_input(code, index, base, par_str, log)
     elif (op == 2):
         index, base = em_multiplication(code, index, base, par_str)
     elif (op == 1):
@@ -195,28 +246,16 @@ def emulate(code, index, base, screen):
         index = -1
     return index, base
 
-def rasterize(screen):
-    score = 0
-    for i in range(0,len(screen),3):
-        x = screen[i]
-        y = screen[i+1]
-        v = screen[i+2]
-        if x == -1 and y == 0:
-            score = v
-    print(score)
-
 raw_intcode = input()
 str_intcode = raw_intcode.split(',')
 intcode = list(map(int, str_intcode))
 for i in range (0,1024):
     intcode.append(0)
-intcode[0] = 2
 
 
 i = 0
 b = 0
-s = list()
+l = [(1,-1)]
 while (i != -1):
-    i, b = emulate(intcode, i, b, s)
+    i, b = emulate(intcode, i, b, l)
 
-rasterize(s)
--- 2019/Day15e.py
+++ 2019/Day15e.py
@@ -1,5 +1,6 @@
 from collections import defaultdict, Counter
 from time import sleep
+from random import randint
 
 def em_adjust_base(code, index, base, par_str):
     if (par_str[0] == '0'):
@@ -86,7 +87,7 @@ def em_jump_if_true(code, index, base, par_str):
 
     return (arg_1 if arg_0 != 0 else index + 3), base
 
-def em_output(code, index, base, par_str, screen):
+def em_output(code, index, base, par_str, map):
     if (par_str[0] == '0'):
         arg_0 = referencedValue(code, index + 1)
     elif (par_str[0] == '2'):
@@ -94,16 +95,16 @@ def em_output(code, index, base, par_str, screen):
     else:
         arg_0 = directValue(code, index + 1)
 
-    screen.append(arg_0)
-    
+    setResult(map, arg_0)
     return (index + 2), base
 
-def em_input(code, index, base, par_str, screen):
+def em_input(code, index, base, par_str, map):
+    move = getMove(map)
 
     if (par_str[2] =='0'):
-        writeReference(code, index + 1, 0)
+        writeReference(code, index + 1, move)
     else:
-        writeRelative(code, index + 1, base, 0)
+        writeRelative(code, index + 1, base, move)
 
     return (index + 2), base
 
@@ -166,7 +167,105 @@ def writeReference(code, index, value):
 def writeRelative(code, index, base, value):
     code[base + code[index]] = value
 
-def emulate(code, index, base, screen):
+def getMove(map):
+    moves = defaultdict(lambda:0)
+    if getUnex(map):
+        return map["LGM"]
+    else:
+        print(floodFill(map))
+        exit()
+
+def setResult(map, result):
+    x, y = map["LOC"]
+    m = map["LGM"]
+
+    if int(m) == 1:
+        x_pot = x
+        y_pot = y - 1
+    elif int(m) == 2:
+        x_pot = x
+        y_pot = y + 1
+    elif int(m) == 3:
+        x_pot = x - 1
+        y_pot = y
+    elif int(m) == 4:
+        x_pot = x + 1
+        y_pot = y
+
+    if int(result) == 0:
+        map[(x_pot, y_pot)] = '█'
+    elif int(result) == 1:
+        map[(x_pot, y_pot)] = '°'
+        x = x_pot
+        y = y_pot
+    else:
+        map[(x_pot, y_pot)] = 'O'
+        x = x_pot
+        y = y_pot
+    
+    map["LOC"] = (x,y)
+    x_min, y_min, x_max, y_max = map["LIM"]
+    map["LIM"] = (min(x, x_min), min(y, y_min), max(x, x_max), max(y, y_max))
+
+def getUnex(map):
+    x, y = map["LOC"]
+    searchSpace = defaultdict(lambda: -1)
+    searchSpace[(x,y)] = (0, [])
+    searching = True
+    while searching:
+        searching = False
+        iterSpace = list(searchSpace.keys())
+        for loc in iterSpace:
+            search, route = searchSpace[loc]
+            if search == 0:
+                searchSpace[loc] = (1, route)
+                x_ser, y_ser = loc
+                for d in range(1,5):
+                    if d == 1:
+                        x_pot = x_ser
+                        y_pot = y_ser - 1
+                    elif d == 2:
+                        x_pot = x_ser
+                        y_pot = y_ser + 1
+                    elif d == 3:
+                        x_pot = x_ser - 1
+                        y_pot = y_ser
+                    elif d == 4:
+                        x_pot = x_ser + 1
+                        y_pot = y_ser
+                    route_pot = route[:]
+                    route_pot.append(d)
+                    if map[(x_pot, y_pot)] == ' ':
+                        map["LGM"] = route_pot[0]
+                        return True
+                    elif map[(x_pot, y_pot)] != '█' and searchSpace[(x_pot, y_pot)] == -1:
+                        searchSpace[(x_pot,y_pot)] = (0,route_pot)
+                        searching = True
+    return False
+
+            
+def neighbours(x, y, map):
+    return [map[(x-1, y)], map[(x+1,y)], map[(x,y-1)], map[(x,y+1)]]
+
+
+def floodFill(map):
+    x_min, y_min, x_max, y_max = map["LIM"]
+    minutes = 0
+    while '°' in map.values():
+        for i in range (y_min, y_max + 1):
+            for j in range (x_min, x_max + 1):
+                if 'O' in neighbours(j, i, map) and map[(j,i)] == '°':
+                    map[(j,i)] = 'o'
+        for i in range (y_min, y_max + 1):
+            for j in range (x_min, x_max + 1):
+                if map[(j,i)] == 'o':
+                    map[(j,i)] = 'O'
+       
+        minutes += 1
+    return minutes
+    
+
+def emulate(code, index, base, map):
     op_str = str(code[index]).zfill(5)
     op = int(op_str[3:])
     par_str = op_str[2::-1]
@@ -183,9 +282,9 @@ def emulate(code, index, base, screen):
     elif (op == 5):
         index, base = em_jump_if_true(code, index, base, par_str)
     elif (op == 4):
-        index, base = em_output(code, index, base, par_str, screen)
+        index, base = em_output(code, index, base, par_str, map)
     elif (op == 3):
-        index, base = em_input(code, index, base, par_str, screen)
+        index, base = em_input(code, index, base, par_str, map)
     elif (op == 2):
         index, base = em_multiplication(code, index, base, par_str)
     elif (op == 1):
@@ -195,28 +294,19 @@ def emulate(code, index, base, screen):
         index = -1
     return index, base
 
-def rasterize(screen):
-    score = 0
-    for i in range(0,len(screen),3):
-        x = screen[i]
-        y = screen[i+1]
-        v = screen[i+2]
-        if x == -1 and y == 0:
-            score = v
-    print(score)
-
 raw_intcode = input()
 str_intcode = raw_intcode.split(',')
 intcode = list(map(int, str_intcode))
 for i in range (0,1024):
     intcode.append(0)
-intcode[0] = 2
 
 
 i = 0
 b = 0
-s = list()
+m = defaultdict(lambda: ' ')
+m['LOC'] = (0,0)
+m['LIM'] = (0,0,0,0)
+m[(0,0)] = '°'
 while (i != -1):
-    i, b = emulate(intcode, i, b, s)
+    i, b = emulate(intcode, i, b, m)
 
-rasterize(s)
