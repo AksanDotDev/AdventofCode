6553ad2d9e04b5af84b922b8e1cd983488d40c82
Alexi Dewfall <git@aksan.dev>
2021-12-13 09:11:20 +0000

    First time I've used del in a while.

---
+++ 21/Day13.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+
+with open("Input13.txt") as raw:
+    folds = []
+    dots = defaultdict(lambda: 0)
+    m_x = 0
+    m_y = 0
+    for line in raw:
+        if line.startswith("fold along"):
+            axis, value = line.strip().split()[2].split("=")
+            folds.append((axis, int(value)))
+        elif line.strip():
+            x, y = map(int, line.strip().split(","))
+            m_x = max(x, m_x)
+            m_y = max(y, m_y)
+            dots[(x, y)] = 1
+
+    for axis, value in folds[:1]:
+        dot_locations = list(dots.keys())
+        if axis == "x":
+            for x, y in dot_locations:
+                if x > value:
+                    dots[(2*value - x, y)] = 1
+                    del dots[(x, y)]
+            m_x = value - 1
+        else:
+            for x, y in dot_locations:
+                if y > value:
+                    dots[(x, 2*value - y)] = 1
+                    del dots[(x, y)]
+            m_y = value - 1
+
+    print(sum(dots.values()))
