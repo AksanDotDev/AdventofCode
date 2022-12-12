ac3ca9b301e6ab48f740077887fd0c3820600774
A J S 'Alexi' Dewfall <apollo@aksan.dev>
2022-12-12 08:44:45 +0000

    Solid B+ for Day 12

---
+++ 22/Day12e.py
@@ -0,0 +1,112 @@
+import sys
+from queue import PriorityQueue
+from dataclasses import dataclass, field
+
+with open(sys.argv[1]) as file:
+    raw = list(map(lambda x: list(x.strip()), file.readlines()))
+
+
+class Coord():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Coord(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Coord(self.x - other.x, self.y - other.y)
+
+    def __repr__(self):
+        return f"Coord({self.x}, {self.y})"
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return isinstance(other, Coord) \
+            and self.x == other.x and self.y == other.y
+
+
+@dataclass(order=True)
+class QItem:
+    priority: int
+    loc: Coord = field(compare=False)
+
+
+n = len(raw)
+m = len(raw[0])
+
+distances = []
+for i in range(n):
+    distances.append([])
+    for j in range(m):
+        distances[i].append(None)
+
+starts = []
+end = Coord(-1, -1)
+
+for i, line in enumerate(raw):
+    for j, char in enumerate(line):
+        if char == "S":
+            distances[i][j] = 0
+            raw[i][j] = "a"
+            starts.append(Coord(i, j))
+        if char == "a":
+            distances[i][j] = 0
+            starts.append(Coord(i, j))
+        elif char == "E":
+            end = Coord(i, j)
+            raw[i][j] = "z"
+
+
+def h(c):
+    o = end - c
+    return abs(o.x) + abs(o.y)
+
+
+def reachable(loc, n):
+    return (ord(raw[n.x][n.y]) - ord(raw[loc.x][loc.y])) <= 1
+
+
+def get_neighbours(loc):
+    result = []
+    options = map(
+        lambda x: x + loc,
+        [
+            Coord(-1, 0),
+            Coord(1, 0),
+            Coord(0, 1),
+            Coord(0, -1)
+        ]
+    )
+    for o in options:
+        if o.x >= 0 and o.x < n and\
+                o.y >= 0 and o.y < m and\
+                (distances[o.x][o.y] is None or
+                 distances[o.x][o.y] > (1 + distances[loc.x][loc.y])) and \
+                reachable(loc, o):
+            result.append(o)
+    return result
+
+
+q = PriorityQueue()
+for start in starts:
+    q.put(QItem(h(start), start))
+end_not_reached = True
+
+while not q.empty() and end_not_reached:
+    expand = q.get().loc
+    expand_dist = distances[expand.x][expand.y]
+    for neighbour in get_neighbours(expand):
+        distances[neighbour.x][neighbour.y] = 1 + expand_dist
+        if neighbour == end:
+            end_not_reached = False
+            break
+        q.put(QItem(h(neighbour) + expand_dist, neighbour))
+
+
+for row in distances:
+    print(row)
+print(distances[end.x][end.y])
