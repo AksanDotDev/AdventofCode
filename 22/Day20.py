cbb126b78f823bef57e9c612b4169e91a3ccb955
Alexi Dewfall <git@aksan.dev>
2022-12-20 08:44:34 +0000

    deques go brrrrr! Day 20

---
+++ 22/Day20.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+with open(sys.argv[1]) as file:
+    raw = list(map(int, file.readlines()))
+
+
+n = len(raw)
+volatile = deque(enumerate(raw))
+
+for idx_i, v in enumerate(raw):
+    idx_v = volatile.index((idx_i, v))
+    volatile.rotate(-1*idx_v)
+    p = volatile.popleft()
+    volatile.rotate(-1*v)
+    volatile.appendleft(p)
+
+frozen = list(map(lambda x: x[1], volatile))
+
+base = frozen.index(0)
+
+
+print(sum(frozen[(i + base) % n] for i in [1000, 2000, 3000]))
---
+++ 22/Day20e.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+with open(sys.argv[1]) as file:
+    raw = list(map(lambda x: 811589153*int(x), file.readlines()))
+
+
+n = len(raw)
+volatile = deque(enumerate(raw))
+
+for _ in range(10):
+    for idx_i, v in enumerate(raw):
+        idx_v = volatile.index((idx_i, v))
+        volatile.rotate(-1*idx_v)
+        p = volatile.popleft()
+        volatile.rotate(-1*v)
+        volatile.appendleft(p)
+
+frozen = list(map(lambda x: x[1], volatile))
+
+base = frozen.index(0)
+
+
+print(sum(frozen[(i + base) % n] for i in [1000, 2000, 3000]))
